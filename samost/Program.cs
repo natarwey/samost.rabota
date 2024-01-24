32d6093b4f079bcfc985b9d0d569083313244790
212201 <212201@1220-012-22>
2024-01-24 10:36:57 +0300

    Добавьте файлы проекта.

---
+++ samost/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static bool IsTrue(string expression)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char c in expression)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                stack.Push(c);
+            }
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                char top = stack.Pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{'))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    static void Main(string[] args)
+    {
+        string expression = Console.ReadLine();
+        bool isTrue = IsTrue(expression);
+        Console.WriteLine($"Выражение \"{expression}\" {(isTrue ? "верно" : "не верно")}");
+    }
+}
